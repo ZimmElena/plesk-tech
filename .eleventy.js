907709be731613502ce7d46cb1c368f15d7f092f
Dmitry Kuznetsov <dkuznetsov@plesk.com>
2022-01-20 15:03:02 +0700

    Updating design and packages

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,8 @@ module.exports = function(config) {
     config.addPassthroughCopy('src/scripts');
     config.addPassthroughCopy('src/**/*.(html|gif|jpg|png|svg|mp4|webm|zip)');
 
+    config.setDataDeepMerge(false);
+
      config.addFilter('addLoadingLazy', (content) => {
         content.replace(/<img(?!.*loading)/g, '<img loading="lazy"');
     });
--- src/scripts/modules/sticky.js
+++ src/scripts/modules/sticky.js
@@ -1,4 +1,4 @@
 window.addEventListener('scroll', () => {
     const header = document.querySelector('header');
-    header.classList.toggle('header--sticky', window.scrollY >= 82);
+    header.classList.toggle('header--sticky', window.scrollY >= header.offsetHeight - 54);
 });
