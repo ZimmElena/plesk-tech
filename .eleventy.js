92ca6598f2d219646512bfeafb37ca723723793b
Dmitry Kuznetsov <dkuznetsov@plesk.com>
2022-02-15 13:08:17 +0700

    Adding page 404 and updating packages

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 module.exports = function(config) {
     config.addPassthroughCopy('src/favicon.ico');
     config.addPassthroughCopy('src/manifest.json');
@@ -88,6 +90,24 @@ module.exports = function(config) {
             return result;
         }
         return content;
+    });
+
+    // Override Browsersync defaults (used only with --serve)
+    config.setBrowserSyncConfig({
+        callbacks: {
+            ready: function(err, browserSync) {
+                const content_404 = fs.readFileSync('dist/404.html');
+
+                browserSync.addMiddleware("*", (req, res) => {
+                    // Provides the 404 content without redirect.
+                    res.writeHead(404, {"Content-Type": "text/html; charset=UTF-8"});
+                    res.write(content_404);
+                    res.end();
+                });
+            },
+        },
+        ui: false,
+        ghostMode: false
     });
 
     return {
